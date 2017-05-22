4ded31052b32b6fa3a8b0b76f7514bb0ecdb4726
Thomas Boulongne <thomasboulongne@hotmail.com>
2017-05-22 17:54:03 +0200

    Add Core Data storage for messages

--- BernieApp/MessageManager.swift
+++ BernieApp/MessageManager.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import Alamofire
+import CoreData
 
 // MARK: - Singleton
 
@@ -30,13 +31,49 @@ final class MessageManager {
             "lang": APIAI_Lang,
             "sessionId": "somerandomthing"
         ]
-    
+            
         Alamofire.request("https://api.api.ai/v1/query?v=20150910", method: .post, parameters: parameters, encoding: JSONEncoding.default, headers: headers).responseJSON { response in
-    
-            if let JSON = response.value {
-                print("JSON: \(JSON)")
+            
+            if let value = response.value {
+                let JSON = value as! Dictionary<String, Any>
+                let result = JSON["result"] as! Dictionary<String, Any>
+                let fulfillment = result["fulfillment"] as! Dictionary<String, Any>
+                let messages = fulfillment["messages"] as! Array<Dictionary<String, Any>>
+                
+                var delay: Double = 0.0
+                
+                for message in messages {
+                    Delay(delay: delay) {
+                        self.processNewMessages(message: message)
+                    }
+                    let speech = message["speech"] as! String
+                    delay = (Double(speech.characters.count)) / 20
+                }
             }
         }
+    }
+    
+    func processNewMessages(message: Dictionary<String, Any>) {
+        
+        let savedMessage = Message(context: self.persistentContainer.viewContext)
+        savedMessage.body = message["speech"] as? String;
+        savedMessage.date = NSDate()
+        self.saveContext()
+        
+    }
+    
+    func getMessages() -> Array<Any> {
+        
+
+        var history: Array<Any> = []
+        
+        do {
+            history = try self.persistentContainer.viewContext.fetch(Message.fetchRequest())
+        } catch {
+            print("Fetching Failed")
+        }
+        
+        return history
     }
     
     // MARK: Shared Instance
@@ -46,5 +83,50 @@ final class MessageManager {
     // MARK: Local Variable
     
     var emptyStringArray : [String] = []
+    
+    // MARK: - Core Data stack
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        /*
+         The persistent container for the application. This implementation
+         creates and returns a container, having loaded the store for the
+         application to it. This property is optional since there are legitimate
+         error conditions that could cause the creation of the store to fail.
+         */
+        let container = NSPersistentContainer(name: "Model")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                
+                /*
+                 Typical reasons for an error here include:
+                 * The parent directory does not exist, cannot be created, or disallows writing.
+                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
+                 * The device is out of space.
+                 * The store could not be migrated to the current model version.
+                 Check the error message to determine what the actual problem was.
+                 */
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    // MARK: - Core Data Saving support
+    
+    func saveContext () {
+        let context = self.persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
     
 }
